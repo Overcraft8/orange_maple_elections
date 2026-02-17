14aff4f06258c3274ba60a59ef23ce09d60e9645
EJ Larracas <EJLarracas@outlook.com>
2026-02-16 20:02:37 -0600

    Mod Files

---
+++ out/html/data.js
@@ -0,0 +1,53 @@
+const tooltipList = [{
+    searchString: "CCF",
+    explanationText: "<img src=img/CCF_logo.jpg> The Co-operative Commonwealth Federation - \n Us"
+}, 
+{
+    searchString: "CP", 
+    explanationText: "The Communist Party"
+},
+{
+    searchString: "LPC", 
+    explanationText: "The Liberal Party"
+},
+{
+    searchString: "RPC", 
+    explanationText: "The Reconstruction Party"
+},
+{
+    searchString: "CPC", 
+    explanationText: "The Conservative Party"
+},
+{
+    searchString: "SCP", 
+    explanationText: "The Social Credit Party"
+}
+];
+
+
+const colourList = [{
+        word: "CCF",
+        style: "color: #D26E28; font-weight: bold;"
+    },
+    {
+        word: "CP", 
+        style: "color: #8B0000; font-weight: bold;"
+    },
+    {
+        word: "LPC", 
+        style: "color: #BE0028; font-weight: bold;"
+    },
+    {
+        word: "RPC", 
+        style: "color: #E7DB73; font-weight: bold;"
+    },
+    {
+        word: "CPC", 
+        style: "color: #0055A5; font-weight: bold;"
+    },
+    {
+        word: "SCP", 
+        style: "color: #50E23D; font-weight: bold;"
+    }
+];
+
