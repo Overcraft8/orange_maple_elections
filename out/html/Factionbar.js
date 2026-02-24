3dfbc8a902b696209850df6e6e4cc39ee551dc05
EJ Larracas <sloganreader@gmail.com>
2026-02-23 18:25:04 -0600

    m

--- out/html/Factionbar.js
+++ out/html/Factionbar.js
@@ -1,3 +1,10 @@
+const factions = [
+  { name: "Agrarians", percent: 40, color: "#a7d11c" },
+  { name: "Labor", percent: 25, color: "#0F52BA" },
+  { name: "Left", percent: 20, color: "#7b1616" },
+  { name: "Reformist", percent: 15, color: "#d26e28" }
+];
+
 function renderFactionBar() {
   const container = document.getElementById("factionBar");
   if (!container) return; // Try again later
